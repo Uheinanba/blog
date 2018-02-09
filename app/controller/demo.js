37ffbeebab81071179019ff88afa81c2068f84d4
heimanba <yk@lingdong365.com>
2018-02-09 15:07:18 +0800

    add demo

--- app/controller/demo.js
+++ app/controller/demo.js
@@ -4,6 +4,12 @@ const Controller = require('egg').Controller;
 
 class Post extends Controller {
   async index() {
+    if (this.a) {
+      this.a = this.a + 1;
+    } else {
+      this.a = 1;
+    }
+    console.log(this.a);
     await this.ctx.render('demo.html', {
       name: this.ctx.authUser ? this.ctx.authUser.name : '',
     });
--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,16 +2,17 @@
 
 module.exports = options => {
   return async function auth(ctx, next) {
-    if (ctx.url.indexOf('blog/server') >= 0) {
-      const user = ctx.service.cookie.getUser();
-      if (user) {
-        ctx.authUser = user;
-        await next();
-      } else {
-        ctx.status = 401; // 未授权
-      }
+    //if (ctx.url.indexOf('blog/server') >= 0) {
+    const user = ctx.service.cookie.getUser();
+    if (user) {
+      ctx.authUser = user;
+      await next();
     } else {
       await next();
+      // ctx.status = 401; // 未授权
     }
+    /* } else {
+      await next();
+    } */
   };
 };
